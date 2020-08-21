b82547518251dcbb0a4c46dffd421635b46a5e59
AlexandrB <burdeinick@yandex.ru>
2020-08-21 11:05:21 +0300

    +doc add_new_group

--- Application/scripts/logic/add_new_group.py
+++ Application/scripts/logic/add_new_group.py
@@ -8,19 +8,19 @@ from scripts.logic.logic import RequestsDb
 
 
 class AddGroup:
-    """ """
+    """This class allows you to add a new group for further processing and polling."""
     def __init__(self):
         self.connect = ConnectionDB()
         self.request_db = RequestsDb()
         self.vk_token = self.connect.get_config_db()[1]
 
     def last_part_url(self, url: str) -> str:
-        """ """
+        """This method leaves only "group_id" for checking."""
         return url.split('/')[-1]
 
 
-    def parser_group(self, url_group: str) -> bool:
-        """ """
+    def request_id_group(self, url_group: str) -> bool:
+        """This method checks the group with a query."""
 
         URL = f"https://api.vk.com/method/groups.getMembers?group_id={url_group}&v=5.122&offset=100&count=10&access_token={self.vk_token}"
         response = requests.get(URL)
@@ -35,7 +35,7 @@ class AddGroup:
         while True:
             inp_url_group = input("Please enter the 'URL' of the 'VK' group: ")
             last_part = self.last_part_url(inp_url_group)
-            pars_gr = self.parser_group(str(last_part))
+            pars_gr = self.request_id_group(str(last_part))
             if pars_gr:
                 resp_add_new_gr = self.request_db.add_new_group(inp_url_group)
                 if resp_add_new_gr:
